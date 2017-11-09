94fe62c991baa2af7b66b62d109530dbe2ce18b8
bleitzel <bleitzel@me.com>
2017-11-09 09:36:25 -0500

    0.0.1

--- app/src/main/java/com/example/bleitzel/gamesapp/MainActivity.java
+++ app/src/main/java/com/example/bleitzel/gamesapp/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_TTT) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_Checkers) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_Chess) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_Roblox) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_Minecraft) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_CODWWII) {
 
         }
 
