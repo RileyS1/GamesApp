dd36bd1000ce41f7f525611cdfd7a1c6f6d1b5b8
bleitzel <bleitzel@me.com>
2017-12-05 10:03:47 -0500

    0.0.5

---
+++ app/src/main/java/com/example/bleitzel/gamesapp/LoginFragment.java
@@ -0,0 +1,11 @@
+package com.example.bleitzel.gamesapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by bleitzel on 12/5/2017.
+ */
+
+public class LoginFragment extends Fragment {
+
+}
--- app/src/main/java/com/example/bleitzel/gamesapp/MainActivity.java
+++ app/src/main/java/com/example/bleitzel/gamesapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.bleitzel.gamesapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,11 +16,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     Fragment contentFragment = null;
+
+    private Button LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        LoginButton = (Button) findViewById(R.id.LoginButton);
+        LoginButtonOnClick loginButtonOnClick = new LoginButtonOnClick();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +53,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private class LoginButtonOnClick implements  View.OnClickListener{
+
+        @Override
+        public void onClick(final View view) {
+            contentFragment = new Fragment();
+        }
     }
 
     @Override
